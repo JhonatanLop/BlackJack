291c45a463645440dcd26277a70a6f5a12f8b560
JhonatanLop <jhooliveira.lopes@gmail.com>
2022-12-18 13:27:29 -0300

    add codigo do game, bkup, readme

---
+++ bkup game.py
@@ -0,0 +1,164 @@
+from utilidades import baralho
+from random import choice
+import sys
+
+# declara valor inicial das fichas
+fichas = 1000.00
+# esvazia a mesa, a mão do usuário e do oponente
+mesa = []
+## player ##
+user_hand = []
+hand_value = 0
+## bot ##
+bot_hand = []
+bot_hand_value = 0
+
+
+resp = False
+
+print("")
+print("olá, bem vindo ou meu projeto de BlackJack")
+jogo = input("gostaria de jogar? s/n\n")
+
+# força a String inteira em letras minúsculas
+jogo = jogo.lower()
+
+while resp == False:
+    if jogo == "s":
+        resp = True
+        break
+    elif jogo == "n":
+        print("startou o programa pq ent porr@ ??")
+    else:
+        print("digite uma opção válida")
+        
+        
+while jogo == "s":
+    
+    ################# parte da aposta ###################
+    # define quanto será a aposta
+    print("")
+    print("Você tem {:.2f} reais em fichas.".format(fichas))
+    
+    if fichas > 0:
+        print("")
+        aposta = float(input("quanto será a posta dessa rodada?  "))
+        if aposta <= fichas:
+            fichas -= aposta
+            premio = 2*aposta
+        else:
+            raise Exception("forçando except")
+        # verifica se a variável é nula
+    else:
+        print("parece que suas fichas acabaram...")
+        print("saia e entre novamente para poder reiniciar a quantidade de fichas")
+        sys.exit()
+    ################# parte do jogo ###################
+    # zera a mão do jogador
+    user_hand = []
+    hand_value = 0
+    bot_hand = []
+    bot_hand_value = 0
+    estourou = False
+    bot_estourou = False
+    
+    print("")
+    print(f"o senhor apostou {aposta:.2f}\nCaso ganhe, o prêmio será de +{aposta}")
+    print("")
+    print(f"aposta: {aposta:.2f}\npremio: {premio}\nfichas: {fichas:.2f}")
+
+    for i in range(2):
+        # transforma o dicionário em uma tupla com 2 valores, para 
+        # variável = escolha(transforma em lista(pega key e valor do dicionário))
+        # retorna 2 valores, a key e o valor
+        random_card = choice(list(baralho.items()))
+        
+        # atribui respectivamente a key da carta e o valor aos valores corretos
+        carta, valor = random_card
+
+        # adiciona carta
+        user_hand.append(carta)
+        hand_value += valor
+        
+        random_card = choice(list(baralho.items()))
+        carta, valor = random_card
+        bot_hand.append(carta)
+        bot_hand_value += valor
+
+
+    print("") # enter
+    print(f"cartas: {user_hand}")
+    print(f"soma: {hand_value}")
+
+    print("")
+    cont = input("gostaria de comprar mais uma carta?\n")
+    while cont == "s":
+        random_card = choice(list(baralho.items()))
+        carta, valor = random_card
+
+        user_hand.append(carta)
+        hand_value += valor
+        
+        print(f"cartas: {user_hand}")
+        print(f"soma: {hand_value}")
+        
+        # verifica se ele já não estourou
+        if hand_value > 21:
+            print("")
+            print("Sua mão estourou!")
+            estourou = True
+            print(f"você perdeu: {aposta}")
+            break
+        elif hand_value == 21:
+            print("")
+            print("21!")
+            print(f"você ganhou: {aposta}")
+            break
+
+        print("")
+        cont = input("gostaria de comprar mais uma carta? s/n\n")
+        
+# agora falta add a possibilidade de jogar contra um bot
+# o bot será simples
+# ele irá continuar comprando cartas até chegar o número de 17
+# isso quer dizer que enquanto o valor na mão dele for menor ou igual a 17 ele continuará comprando cartas
+
+    while bot_hand_value < 16:
+        random_card = choice(list(baralho.items()))
+        carta, valor = random_card
+        bot_hand.append(carta)
+        bot_hand_value += valor
+        if bot_hand_value > 21:
+            bot_estourou = True
+            break
+
+    #### final do game ####
+    print("")
+    # imprime mensagens de felicidade, caso tenha ganhado e de tristesa, caso tenha perdido.
+    if bot_estourou and estourou == False:
+        print(f"sua mão: {user_hand}, soma: {hand_value}\nmão do bot: {bot_hand}, soma: {bot_hand_value}")
+        print(f"Parabéns! você ganhou {aposta}\n")
+        fichas += premio
+    elif estourou and bot_estourou == False:
+        print(f"sua mão: {user_hand}, soma: {hand_value}\nmão do bot: {bot_hand}, soma: {bot_hand_value}")
+        print("")
+        print(f"Você estourou e perdeu {aposta}... jogue novamente para se recuperar.")
+    elif hand_value > bot_hand_value and estourou == False:
+        fichas += premio
+        print(f"sua mão: {user_hand}, soma: {hand_value}\nmão do bot: {bot_hand}, soma: {bot_hand_value}")
+        print(f"Parabéns! você ganhou {aposta}\n")
+    elif bot_hand_value > hand_value and bot_estourou == False:
+        print(f"sua mão: {user_hand}, soma: {hand_value}\nmão do bot: {bot_hand}, soma: {bot_hand_value}")
+        print("")
+        print(f"Você perdeu {aposta}...jogue novamente para se recuperar.")
+    elif bot_estourou == True and estourou == True:
+        print(f"sua mão: {user_hand}, soma: {hand_value}\nmão do bot: {bot_hand}, soma: {bot_hand_value}")
+        print(f"Empate!, jogue novamente")
+        fichas += aposta
+    else:
+        print(f"sua mão: {user_hand}, soma: {hand_value}\nmão do bot: {bot_hand}, soma: {bot_hand_value}")
+        print(f"Empate!, jogue novamente")
+        fichas += aposta
+
+    # caso seja digitado qualquer coisa != s, a partida irá encerrar  
+    jogo = input("\njogar novamente? s/n  ")
---
+++ readme.md
@@ -0,0 +1,69 @@
+# BlackJack Game ♣️
+
+> Status: Em desenvolvimento ⏱️
+
+  O objetivo desse projeto é aprender mais sobre python. Por ser algo que tem capacidade de explorar muito das funções, acho interessenta começar com um jogo simples de blackjack, e ir aprimorando essa idéia.
+
+  A primeira versão do jogo já está pronta. O jogo é executádo via terminal e para seu funcionamento os arquivos ***game.py*** e ***utilidades.py*** devem estar num  mesmo diretório dentro do seu pc. 
+
+## Como funciona? ⚙️
+Ao invéz do modo tradicional, nessa versão o jogador compra as cartas dele primeiro e quando decidir que já deu, o bot irá comprar e continuar comprando cartas até que a mão dele tenha no mínimo 16.
+
+Seus status como cartas e soma, são exibidas durante o jogo. No final, sua mão é comparada com a do adversário e um resultado é anunciado junto com a distribuição de fichas.
+
+Por enquanto não possível jogar contra mais de um oponente...
+
+## Como jogar? ♟️
+Ao iniciar, é perguntado se você deseja jogar... Essa parte do código não é essencial para o funcionamento do game, mas acabei deixando mesmo assim.<br>
+Caso a resposta seja *sim*, o jogo começa.<br>
+Caso a resposta seja *não*, uma mensagem gentil é exibida.<br>
+> As respostas em sí são dados como "*s*" ou "*n*"
+
+* ### Aposta  💰
+
+> O algoritmo pede para você fornecer um valor a ser apostado, que seja menor ou igual ao número de fichas que você possui. <br>Automaticamente, a quantidade de fichas que você apostou é retirada do seu "cofre". <br>
+> * ⚠️  Vale lembrar que só é permitido fazer **UMA** aposta dentro do game, que é a inicial. ⚠️</p>
+
+* ### Compra de Cartas  ♠️
+
+> Caso você ainda não esteja confiante com a sua mão, é possível comprar mais cartas. <br>Dentro do game existe uma ***trigger*** que é ativada quando sua mão "estoura". Não sendo possível comprar mais cartas ou fazer apostas
+> Quando a soma de suas cartas é == 21, a compra de cartas não estará mais disponível<br>
+> Ao finalizar sua compra de cartas, é a vez do oponente (*que também possui a mesma **trigger***)<br>
+
+* ### Ganhar ou perder?  👊
+
+> O jogo possui 4 tipos de validações:
+> * sua mão é maior que a do adversário **E** sua mão não "estourou"
+> * sua mão e menor que a do adversário **E** a mão dele não "estourou"
+> * sua mão é igual que a do adversário
+> * as duas mãos estouraram<br>
+> 
+> Caso você ganhe, recebe o dobro do valor que apostou, exemplo:<br>
+> Supondo que você tenha apostado 500, esse valor será retirado da sua quantidade total de fichas. Se ganhar, essas fichas voltam para você com mais 500. O dobro que apostou.
+
+* ### Como rodar o jogo? 🕹️
+
+> 1. Você pode clonar esse repositório na sua máquina ou copiar os dois arquivos mencionados acima (*utilidades.py* e *game.py*).
+> 2. Abra o terminal no diretório que os arquivos foram baixados ou copiados *(ambos precisam estar no mesmo diretório)*
+> 3. Caso você tenha o python instalado na sua máquina, Digite no terminal python:
+
+~~~ 
+python game.py 
+~~~
+
+## Futuras melhorias 🚀
+
+* Atualmente o código do arquivo principal do jogo "***game.py***", possui 163 linhas, incluindo comentários.
+Planejo reduzir a repetição de elementos dentro do código usando funções.<br>
+> O arquivo *utilidades.py* tinha sido criado inicialmente para essa funcionalidade que vai acabar saindo para uma versão 2.0 do game.
+* Planejo adicionar a possibilidade de jogar contra mais de um oponente e poder ver a primeira carta de todos os jogadores (*assim como no jogo tradicional*)
+
+
+
+## Agradecimentos ✌️
+
+> *Obrigado por acessar meu github e conhecer meu projeto. Caso queira enviar sugestões meu email e instagram estarão logo abaixo*
+> <br>
+>
+>
+> <a href = "mailto:jhooliveira.lopes@gmail.com"><img src="https://img.shields.io/badge/-Gmail-%23333?style=for-the-badge&logo=gmail&logoColor=white" target="_blank"></a> <a href="https://www.instagram.com/jhonatan_lopes_lmao/?next=%2F" target="_blank"><img src="https://img.shields.io/badge/-Instagram-%23E4405F?style=for-the-badge&logo=instagram&logoColor=white" target="_blank"></a> 
